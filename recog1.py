d88c4467d633bdfd2c62ffd86188dadda3f5f2cd
unknown <yuw688@ucsd.edu>
2018-05-05 21:58:51 -0700

    Add keyword recognition

---
+++ recog1.py
@@ -0,0 +1,69 @@
+word_book = open('wordlist_selected.txt','r')
+positive_book = open('positive-words.txt','r')
+negative_book = open('negative-words.txt','r')
+keywords = [x[0:-1].lower() for x in word_book]
+positive = [x[0:-1].lower() for x in positive_book]
+negative = [x[0:-1].lower() for x in negative_book]
+keywords.append('feel')
+def textrecog(text_list):
+    shorter = []
+    keyword = []
+    determine = {}
+    result = {}
+    for word in text_list:
+        if len(word) > 1:
+            shorter.append(word)
+    for i in range(len(shorter)):
+        w = shorter[i]
+        if w in negative:
+            determine[i] = 0
+        elif w in positive:
+            determine[i] = 1
+        elif w.isdigit():
+            determine[i] = 2
+        if w in keywords:
+            keyword.append(i)
+    key = []
+    for i in determine.keys():
+        key.append(i)
+    for index in range(len(keyword)):
+        i = keyword[index]
+        choose = -1
+        for j in key:
+            if (j - i > 0 or i - j == 1):
+                if index + 1 == len(keyword):
+                    choose = j
+                    break
+                elif j <= keyword[index + 1]:
+                    choose = j
+                    break
+        if choose != -1 and determine[choose] == 0:
+            if shorter[i] not in result.keys():
+                result[shorter[i]] = shorter[choose]
+        elif choose != -1 and determine[choose] == 1:
+            if (choose+1) in key:
+                if determine[choose+1] == 2:
+                    if shorter[i] not in result.keys():
+                        result[shorter[i]] = shorter[choose+1]
+            elif (choose+2) in key:
+                if determine[choose+2] == 2:
+                    if shorter[i] not in result.keys():
+                        result[shorter[i]] = shorter[choose+2]
+            else:
+                if shorter[i] not in result.keys():
+                    result[shorter[i]] = shorter[choose]
+        elif choose != -1 and determine[choose] == 2:
+            if shorter[i] not in result.keys():
+                result[shorter[i]] = shorter[choose]
+    return result
+
+def dataprocess(text):
+    text_list = text.split()
+    result = textrecog(text_list)
+    result_rev = textrecog(text_list[::-1])
+    for key in result_rev.keys():
+        if key not in result.keys():
+            result[key] = result_rev[key]
+    return result
+print(dataprocess('I feel bad what your height my height is 180 cm and what about your fevor my fever get worse I get a bad cold I broke my leg'))
+ 
